7d3d153eeb2000751da4a263dbb92fd6836e78d8
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-06-29 14:30:40 +0530

    Create Write a program to check whether the given number as input is positive, negative, or zero..cpp
    Positive and Negative

---
+++ Write a program to check whether the given number as input is positive, negative, or zero..cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    
+    if( num < 0) {
+    cout << "negative";
+    }
+    else if ( num > 0) {
+    cout << "positive";
+    }
+    else
+    cout << "zero";
+
+    return 0;
+}
