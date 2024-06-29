504ebd4e4b3ebb06cab67f7010ca842d7b6617f9
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-06-29 10:55:19 +0530

    Create Output on a single line the total number of courses in the section..cpp

---
+++ Output on a single line the total number of courses in the section..cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int language, total_course;
+	int course =2;
+	cin >> language;
+	total_course = 2* language;
+	cout <<  total_course;
+	return 0;
+
+}
+
+
+//Input Format
+//The only line of input will contain a single integer 
+//𝑁
+//N, denoting the number of languages for which there are courses.
+
+//Output Format
+//Output on a single line the total number of courses in the section.
+
