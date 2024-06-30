10c8f2efc45b5742c4665e56fce539099a8573cb
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-06-30 10:08:01 +0530

    Create Alice and Marks.cpp
    Alice and Marks
    Alice has scored 
    𝑋
    X marks in her test and Bob has scored 
    𝑌
    Y marks in the same test. Alice is happy if she scored at least twice the marks of Bob’s score. Determine whether she is happy or not.
    
    Input Format
    The first and only line of input contains two space-separated integers 
    𝑋
    ,
    𝑌
    X,Y — the marks of Alice and Bob respectively.
    Output Format
    For each testcase, print Yes if Alice is happy and No if she is not, according to the problem statement.
    
    The judge is case insensitive so you may output the answer in any case. In particular YES, yes, yEsare all considered equivalent toYes`.
    
    Constraints
    1
    ≤
    𝑋
    ,
    𝑌
    ≤
    100
    1≤X,Y≤100

---
+++ Alice and Marks.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+	// your code goes here
+	if ( x >= (2*y)) {
+	    cout <<"YES";
+	}
+	else
+	{
+	    cout <<"NO";
+	}
+	return 0;
+}
