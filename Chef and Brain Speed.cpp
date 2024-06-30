9f242e1832a0c6b66d907e46d6ad308b2b5b4c6d
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-06-30 09:59:31 +0530

    Create Chef and Brain Speed.cpp
    Chef and Brain Speed
    In ChefLand, human brain speed is measured in bits per second (bps). Chef has a threshold limit of 
    𝑋
    X bits per second above which his calculations are prone to errors. If Chef is currently working at 
    𝑌
    Y bits per second, is he prone to errors?
    
    If Chef is prone to errors print YES, otherwise print NO.
    
    Input Format
    The only line of input contains two space separated integers 
    𝑋
    X and 
    𝑌
    Y — the threshold limit and the rate at which Chef is currently working at.
    
    Output Format
    If Chef is prone to errors print YES, otherwise print NO.
    
    You may print each character of the string in uppercase or lowercase (for example, the strings yes, Yes, yEs, and YES will all be treated as identical).
    
    Constraints
    1
    ≤
    𝑋
    ,
    𝑌
    ≤
    100
    1≤X,Y≤100

---
+++ Chef and Brain Speed.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y; // y is current speed & x is threshold
+    cin>>x>>y;
+    if ( y > x ){
+        cout <<"YES";
+    }
+    else {
+        cout <<"NO";
+    }
+
+	return 0;
+}
+
+
