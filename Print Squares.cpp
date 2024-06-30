c5f5bb62c222bc42c7857d19d96cad585a8547bc
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-06-30 14:49:41 +0530

    Create Print Squares.cpp
    Print Squares
    Write a program that utilizes a while loop to print the squares of numbers from 1 to 
    𝑁
    N.
    
    Check the sample input / output below further clarity.
    
    Sample 1:
    Input
    Output
    5
    1 4 9 16 25

---
+++ Print Squares.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write your code here
+    int x;
+    cin >> x;
+    int y = 1;
+    while ( y <= x ){
+        cout << y*y <<" ";
+        y++;
+    }
+    return 0;
+}
