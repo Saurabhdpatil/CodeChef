035c146c42938613cfc8f6680c8dbd40a6ede2dd
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-07-05 13:47:11 +0530

    Create Lucky Seven.cpp
    Lucky Seven
    Chef considers the number 
    7
    7 lucky. As a result, he believes that the 
    7
    7-th letter he sees on a day is his lucky letter of the day.
    
    You are given a string 
    𝑆
    S of length 
    10
    10, denoting the first 
    10
    10 letters Chef saw today.
    What is Chef's lucky letter?
    
    Input Format
    The only line of input contains a string 
    𝑆
    S, of length 
    10
    10.
    Output Format
    Print a single character: Chef's lucky letter .
    Constraints
    𝑆
    S has a length of 
    10
    10
    𝑆
    S contains only lowercase Latin letters (i.e, the characters 'a' to 'z')
    Sample 1:
    Input
    Output
    proceeding
    d
    Explanation:
    The 
    7
    7-th character of 
    "proceeding"
    "proceeding" is 'd', and hence that is Chef's lucky letter.
    
    Sample 2:
    Input
    Output
    outofsight
    i
    Explanation:
    The 
    7
    7-th character of 
    "outofsight"
    "outofsight" is 'i', and hence that is Chef's lucky letter.

---
+++ Lucky Seven.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string str;
+	cin>>str;
+	int string =str.length();
+	if(string >= 7){
+	    char x=str[6];
+	    cout<<x;
+	}
+	return 0;
+}
