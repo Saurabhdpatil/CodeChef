1eaab56e4860b40d52e22a285f475c4556a3418d
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-07-06 01:05:46 +0530

    Create Who is taller!.cpp
    Who is taller!
    Alice and Bob were having an argument about which of them is taller than the other. Charlie got irritated by the argument, and decided to settle the matter once and for all.
    
    Charlie measured the heights of Alice and Bob, and got to know that Alice's height is 
    𝑋
    X centimeters and Bob's height is 
    𝑌
    Y centimeters. Help Charlie decide who is taller.
    
    It is guaranteed that 
    𝑋
    ≠
    𝑌
    X=Y.
    
    Input Format
    The first line of input will contain an integer 
    𝑇
    T — the number of test cases. The description of 
    𝑇
    T test cases follows.
    The first and only line of each test case contains two integers 
    𝑋
    X and 
    𝑌
    Y, as described in the problem statement.
    Output Format
    For each test case, output on a new line 
    A
    A if Alice is taller than Bob, else output 
    B
    B. The output is case insensitive, i.e, both 
    A
    A and 
    a
    a will be accepted as correct answers when Alice is taller.
    
    Constraints
    1
    ≤
    𝑇
    ≤
    1000
    1≤T≤1000
    100
    ≤
    𝑋
    ,
    𝑌
    ≤
    200
    100≤X,Y≤200
    𝑋
    ≠
    𝑌
    X=Y
    Sample 1:
    Input
    Output
    2
    150 160
    160 150
    B
    A
    Explanation:
    Test case 
    1
    1: In this case, 
    150
    <
    160
    150<160 so Bob is taller than Alice.
    
    Test case 
    2
    2: In this case, 
    160
    >
    150
    160>150 so Alice is taller than Bob.

---
+++ Who is taller!.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,A,B;
+	cin>>t;
+	while (t--){
+	    cin>>A>>B;
+	    if (A>B){
+	        cout<<"A"<<endl;
+	    }
+	    else{
+	        cout<<"B"<<endl;
+	}
+	    
+	}
+    return 0;
+}
