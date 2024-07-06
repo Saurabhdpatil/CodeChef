75ac9d4f689db65d3a01b325acf24674bc062f36
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-07-07 02:55:26 +0530

    Create Reach the Target.cpp
    Reach the Target
    There is a cricket match going on between two teams 
    𝐴
    A and 
    𝐵
    B.
    
    Team 
    𝐵
    B is batting second and got a target of 
    𝑋
    X runs. Currently, team 
    𝐵
    B has scored 
    𝑌
    Y runs. Determine how many more runs Team 
    𝐵
    B should score to win the match.
    
    Note: The target score in cricket matches is one more than the number of runs scored by the team that batted first.
    
    Input Format
    The first line of input will contain a single integer 
    𝑇
    T, denoting the number of test cases.
    Each test case consists of two space-separated integers 
    𝑋
    X and 
    𝑌
    Y, the target for team 
    𝐵
    B and the current score of team 
    𝐵
    B respectively.
    Output Format
    For each test case, output how many more runs team 
    𝐵
    B should score to win the match.
    
    Constraints
    1
    ≤
    𝑇
    ≤
    10
    1≤T≤10
    50
    ≤
    𝑌
    <
    𝑋
    ≤
    200
    50≤Y<X≤200
    Sample 1:
    Input
    Output
    4
    200 50
    100 99
    130 97
    53 51
    150
    1
    33
    2
    Explanation:
    Test case 
    1
    1: The target is 
    200
    200 runs and team 
    𝐵
    B has already made 
    50
    50 runs. Thus, the team needs to make 
    200
    −
    50
    =
    150
    200−50=150 runs more, to win the match.
    
    Test case 
    2
    2: The target is 
    100
    100 runs and team 
    𝐵
    B has already made 
    99
    99 runs. Thus, the team needs to make 
    100
    −
    99
    =
    1
    100−99=1 runs more, to win the match.
    
    Test case 
    3
    3: The target is 
    130
    130 runs and team 
    𝐵
    B has already made 
    97
    97 runs. Thus, the team needs to make 
    130
    −
    97
    =
    33
    130−97=33 runs more, to win the match.
    
    Test case 
    4
    4: The target is 
    53
    53 runs and team 
    𝐵
    B has already made 
    51
    51 runs. Thus, the team needs to make 
    53
    −
    51
    =
    2
    53−51=2 runs more, to win the match.

---
+++ Reach the Target.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    int target=x-y;
+	    cout<<target<<endl;
+	}
+	return 0;
+}
