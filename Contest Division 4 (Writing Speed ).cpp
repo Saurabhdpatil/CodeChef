a87fb76a89327a9295e82e17e7919043d873441d
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-07-10 23:09:42 +0530

    Create Contest Division 4 (Writing Speed ).cpp
    Writing Speed
    Rahul has a 
    5
    5-page assignment due in 
    60
    60 minutes. He can write one page in 
    𝑋
    X minutes.
    
    Determine if Rahul can complete the assignment within the given time constraint.
    
    Input Format
    The only line of input will contain a single integer 
    𝑋
    X, denoting the time taken (in minutes) by Rahul to write one page.
    Output Format
    Print YES if Rahul can complete the assignment in time, otherwise print NO.
    
    You may print each character of the output in either uppercase or lowercase (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
    
    Constraints
    1
    ≤
    𝑋
    ≤
    1000
    1≤X≤1000
    Sample 1:
    Input
    Output
    5
    YES
    Explanation:
    If Rahul can write one page in 
    5
    5 minutes, then he can write 
    5
    5 pages in 
    25
    25 minutes. So, he will be able to complete the assignment in time.
    
    Sample 2:
    Input
    Output
    45
    NO
    Explanation:
    If Rahul can write one page in 
    45
    45 minutes, then he will write 
    5
    5 pages in 
    225
    225 minutes. So, he will not be able to complete the assignment in time.

---
+++ Contest Division 4 (Writing Speed ).cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	int sum = 5 * x;
+	if (sum <= 60){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
