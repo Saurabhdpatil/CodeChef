a4bae8f783a0dc39e4c7ccad8eb59aa9c972c684
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-07-10 13:06:57 +0530

    Create Tom and Jerry Chase.cpp
    Tom and Jerry Chase
    In a classic chase, Tom is running after Jerry as Jerry has eaten Tom's favourite food.
    
    Jerry is running at a speed of 
    𝑋
    X metres per second while Tom is chasing him at a speed of 
    𝑌
    Y metres per second. Determine whether Tom will be able to catch Jerry.
    
    Note that initially Jerry is not at the same position as Tom.
    
    Input Format
    The first line of input will contain a single integer 
    𝑇
    T, denoting the number of test cases.
    Each test case consists of two space-separated integers 
    𝑋
    X and 
    𝑌
    Y — the speeds of Jerry and Tom respectively.
    Output Format
    For each test case, output on a new line, YES, if Tom will be able to catch Jerry. Otherwise, output NO.
    
    You can print each character in uppercase or lowercase. For example NO, no, No, and nO are all considered the same.
    
    Constraints
    1
    ≤
    𝑇
    ≤
    100
    1≤T≤100
    1
    ≤
    𝑋
    ,
    𝑌
    ≤
    10
    1≤X,Y≤10
    Sample 1:
    Input
    Output
    4
    2 3
    4 1
    1 1
    3 5
    YES
    NO
    NO
    YES
    Explanation:
    Test case 
    1
    1: Jerry is running at the speed of 
    2
    2 metres per second while Tom is chasing him at the speed of 
    3
    3 metres per second. Since Jerry's speed is less than Tom's, Tom will eventually catch Jerry.
    
    Test case 
    2
    2: Jerry is running at the speed of 
    4
    4 metres per second while Tom is chasing him at the speed of 
    1
    1 metres per second. Since Jerry's speed is higher than Tom's, Tom will never be able to catch Jerry.
    
    Test case 
    3
    3: Jerry is running at the speed of 
    1
    1 metre per second while Tom is chasing him at the speed of 
    1
    1 metre per second. Since Jerry's speed is same as that of Tom's and both of them are not at the same position, Tom will never be able to catch Jerry.
    
    Test case 
    4
    4: Jerry is running at the speed of 
    3
    3 metres per second while Tom is chasing him at the speed of 
    5
    5 metres per second. Since Jerry's speed is less than Tom's, Tom will eventually catch Jerry.

---
+++ Tom and Jerry Chase.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    if (y>x){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+
+}
