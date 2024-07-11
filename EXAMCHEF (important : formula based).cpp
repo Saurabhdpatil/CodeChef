19db82ada61d15ea176496fbbcea0f2556127dd8
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-07-12 01:17:42 +0530

    Create EXAMCHEF (important : formula based).cpp
    Exams
    In Chefland, there are 
    𝑋
    X schools, and each school has 
    𝑌
    Y students.
    The year end results are in and a total of 
    𝑍
    Z students passed the exams.
    
    Assuming that all students appeared for the exams, find whether the number of students who passed in Chefland was strictly greater than 
    50
    %
    50%.
    
    Input Format
    The first line of input will contain a single integer 
    𝑇
    T, denoting the number of test cases.
    Each test case consists of three space-separated integers 
    𝑋
    ,
    𝑌
    ,
    X,Y, and 
    𝑍
    Z, as mentioned in the statement.
    Output Format
    For each test case, output on a new line, YES, if the total number of students who passed in Chefland was strictly greater than 
    50
    %
    50%, otherwise print NO.
    
    You may print each character of the string in uppercase or lowercase (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
    
    Constraints
    1
    ≤
    𝑇
    ≤
    2
    ⋅
    1
    0
    4
    1≤T≤2⋅10 
    4
     
    1
    ≤
    𝑋
    ≤
    5
    1≤X≤5
    1
    ≤
    𝑌
    ≤
    50
    1≤Y≤50
    0
    ≤
    𝑍
    ≤
    𝑋
    ⋅
    𝑌
    0≤Z≤X⋅Y
    Sample 1:
    Input
    Output
    4
    2 10 12
    2 10 3
    1 5 3
    3 6 9
    YES
    NO
    YES
    NO
    Explanation:
    Test case 
    1
    1: The total number of students appeared were 
    2
    ⋅
    10
    =
    20
    2⋅10=20. The number of students passed were 
    12
    12.
    Thus, number of students who passed are 
    60
    %
    60%, which is strictly greater than 
    50
    %
    50%.
    
    Test case 
    2
    2: The total number of students appeared were 
    2
    ⋅
    10
    =
    20
    2⋅10=20. The number of students passed were 
    3
    3.
    Thus, number of students who passed are 
    15
    %
    15%, which is less than 
    50
    %
    50%.
    
    Test case 
    3
    3: The total number of students appeared were 
    1
    ⋅
    5
    =
    5
    1⋅5=5. The number of students passed were 
    3
    3.
    Thus, number of students who passed are 
    60
    %
    60%, which is strictly greater than 
    50
    %
    50%.
    
    Test case 
    4
    4: The total number of students appeared were 
    3
    ⋅
    6
    =
    18
    3⋅6=18. The number of students passed were 
    9
    9.
    Thus, number of students who passed are 
    50
    %
    50%, which is equal to 
    50
    %
    50%.

---
+++ EXAMCHEF (important : formula based).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x,y,z,t;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y>>z;
+	    x = x * y;
+	    if( z > x/2){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+
+}
