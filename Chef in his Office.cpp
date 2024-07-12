c10cdef046e758ff147758a498e754c1bbb6f0ef
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-07-12 11:00:24 +0530

    Create Chef in his Office.cpp
    Chef in his Office
    Recently Chef joined a new company. In this company, the employees have to work for 
    𝑋
    X hours each day from Monday to Thursday. Also, in this company, Friday is called Chill Day — employees only have to work for 
    𝑌
    Y hours 
    (
    𝑌
    <
    𝑋
    )
    (Y<X) on Friday. Saturdays and Sundays are holidays.
    
    Determine the total number of working hours in one week.
    
    Input Format
    The first line contains a single integer 
    𝑇
    T — the number of test cases. Then the test cases follow.
    The first and only line of each test case contains two space-separated integers 
    𝑋
    X and 
    𝑌
    Y — the number of working hours on each day from Monday to Thursday and the number of working hours on Friday respectively.
    Output Format
    For each test case, output the total number of working hours in one week.
    
    Constraints
    1
    ≤
    𝑇
    ≤
    100
    1≤T≤100
    2
    ≤
    𝑋
    ≤
    12
    2≤X≤12
    1
    ≤
    𝑌
    <
    𝑋
    1≤Y<X
    Sample 1:
    Input
    Output
    3
    10 5
    12 2
    8 7
    45
    50
    39
    Explanation:
    Test case 
    1
    1: The total number of working hours in a week are: 
    10
    (Monday)
    +
    10
    (Tuesday)
    +
    10
    (Wednesday)
    +
    10
    (Thursday)
    +
    5
    (Friday)
    =
    45
    10(Monday)+10(Tuesday)+10(Wednesday)+10(Thursday)+5(Friday)=45
    
    Test Case 2: The total number of working hours in a week are: 
    12
    (Monday)
    +
    12
    (Tuesday)
    +
    12
    (Wednesday)
    +
    12
    (Thursday)
    +
    2
    (Friday)
    =
    50
    12(Monday)+12(Tuesday)+12(Wednesday)+12(Thursday)+2(Friday)=50
    
    Test Case 3: The total number of working hours in a week are: 
    8
    (Monday)
    +
    8
    (Tuesday)
    +
    8
    (Wednesday)
    +
    8
    (Thursday)
    +
    7
    (Friday)
    =
    39
    8(Monday)+8(Tuesday)+8(Wednesday)+8(Thursday)+7(Friday)=39

---
+++ Chef in his Office.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    int hours = x * 4 + y;
+	    cout<<hours<<endl;
+	}
+	return 0;
+}
