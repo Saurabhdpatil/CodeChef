6dfba1f6301c9d8e8c9d29d000e3399a75d99743
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-07-15 13:44:57 +0530

    Create Maximize Bags from CodeChef Store.cpp
    CRED Coins
    For each bill you pay using CRED, you earn 
    𝑋
    X CRED coins.
    At CodeChef store, each bag is worth 
    100
    100 CRED coins.
    
    Chef pays 
    𝑌
    Y number of bills using CRED. Find the maximum number of bags he can get from the CodeChef store.
    
    Input Format
    First line will contain 
    𝑇
    T, number of test cases. Then the test cases follow.
    Each test case contains of a single line of input, two integers 
    𝑋
    X and 
    𝑌
    Y.
    Output Format
    For each test case, output in a single line - the maximum number of bags Chef can get from the CodeChef store.
    
    Constraints
    1
    ≤
    𝑇
    ≤
    100
    1≤T≤100
    1
    ≤
    𝑋
    ,
    𝑌
    ≤
    1000
    1≤X,Y≤1000
    Subtasks
    Subtask 1 (100 points): Original constraints.
    Sample 1:
    Input
    Output
    3
    10 10
    20 4
    70 7
    1
    0
    4
    Explanation:
    Test Case 
    1
    1: For each bill payment, one receives 
    10
    10 CRED coins. Chef pays 
    10
    10 bills using CRED. Thus, he receives 
    100
    100 CRED coins. Chef can get 
    1
    1 bag from the CodeChef store using these coins.
    
    Test Case 
    2
    2: For each bill payment, one receives 
    20
    20 CRED coins. Chef pays 
    4
    4 bills using CRED. Thus, he receives 
    80
    80 CRED coins. Chef cannot get any bag from the CodeChef store using these coins.
    
    Test Case 
    3
    3: For each bill payment, one receives 
    70
    70 CRED coins. Chef pays 
    7
    7 bills using CRED. Thus, he receives 
    490
    490 CRED coins. Chef can get at most 
    4
    4 bags from the CodeChef store using these coins.

---
+++ Maximize Bags from CodeChef Store.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T,X,Y;
+	cin>>T;
+	while(T--){
+	    cin>>X>>Y;
+	    int max = (X * Y)/100;   // maximum number of bags Chef can get from the CodeChef store.
+	    cout<<max<<endl;
+	}
+
+}
