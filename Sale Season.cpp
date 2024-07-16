50d590c256d9ed5538da8117e1d2c5af54902eea
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-07-16 17:21:44 +0530

    Create Sale Season.cpp
    Sale Season
    It's the sale season again and Chef bought items worth a total of 
    𝑋
    X rupees. The sale season offer is as follows:
    
    if 
    𝑋
    ≤
    100
    X≤100, no discount.
    if 
    100
    <
    𝑋
    ≤
    1000
    100<X≤1000, discount is 
    25
    25 rupees.
    if 
    1000
    <
    𝑋
    ≤
    5000
    1000<X≤5000, discount is 
    100
    100 rupees.
    if 
    𝑋
    >
    5000
    X>5000, discount is 
    500
    500 rupees.
    Find the final amount Chef needs to pay for his shopping.
    
    Input Format
    The first line of input will contain a single integer 
    𝑇
    T, denoting the number of test cases.
    Each test case consists of single line of input containing an integer 
    𝑋
    X.
    Output Format
    For each test case, output on a new line the final amount Chef needs to pay for his shopping.
    
    Constraints
    1
    ≤
    𝑇
    ≤
    100
    1≤T≤100
    1
    ≤
    𝑋
    ≤
    10000
    1≤X≤10000
    Sample 1:
    Input
    Output
    4
    15
    70
    250
    1000
    15
    70
    225
    975
    Explanation:
    Test case 
    1
    1: Since 
    𝑋
    ≤
    100
    X≤100, there is no discount.
    
    Test case 
    3
    3: Here, 
    𝑋
    =
    250
    X=250. Since 
    100
    <
    250
    ≤
    1000
    100<250≤1000, discount is of 
    25
    25 rupees. Therefore, Chef needs to pay 
    250
    −
    25
    =
    225
    250−25=225 rupees.

---
+++ Sale Season.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    if(100 < x && x <= 1000){
+	        cout<<x-25<<endl;
+	}
+	else if (1000 < x && x <= 5000){
+	    cout<<x-100<<endl;
+	}
+	else if(x > 5000){
+	    cout<<x-500<<endl;
+	}
+	else{
+	    cout<<x<<endl;
+	}
+}
+    
+}
