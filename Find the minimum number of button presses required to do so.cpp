a7c86e41d1f8fd1d8439c2e6e8556505307256a2
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-08-10 15:47:35 +0530

    Create Find the minimum number of button presses required to do so.cpp
    Volume Control
    Chef is watching TV. The current volume of the TV is 
    X
    X. Pressing the volume up button of the TV remote increases the volume by 
    1
    1 while pressing the volume down button decreases the volume by 
    1
    1. Chef wants to change the volume from 
    X
    X to 
    Y
    Y. Find the minimum number of button presses required to do so.
    
    Input Format
    The first line contains a single integer 
    T
    T - the number of test cases. Then the test cases follow.
    The first and only line of each test case contains two integers 
    X
    X and 
    Y
    Y - the initial volume and final volume of the TV.
    Output Format
    For each test case, output the minimum number of times Chef has to press a button to change the volume from 
    X
    X to 
    Y
    Y.
    
    Constraints
    1
    ≤
    T
    ≤
    100
    1≤T≤100
    1
    ≤
    X
    ,
    Y
    ≤
    100
    1≤X,Y≤100
    Sample 1:
    Input
    Output
    2
    50 54
    12 10
    4
    2
    Explanation:
    Test Case 1: Chef can press the volume up button 
    4
    4 times to increase the volume from 
    50
    50 to 
    54
    54.
    
    Test Case 2: Chef can press the volume down button 
    2
    2 times to decrease the volume from 
    12
    12 to 
    10
    10.

---
+++ Find the minimum number of button presses required to do so.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    if(x>y){
+	        int tot=x-y;
+	        cout<<tot<<endl;
+	    }
+	    else {
+	        int tot=y-x;
+	        cout<<tot<<endl;
+	    }
+	    
+	}
+	return 0;
+}
