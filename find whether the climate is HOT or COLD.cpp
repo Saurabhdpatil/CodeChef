889551371280ce79065c5fc6bb28359aea2e4f9e
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-08-11 23:21:26 +0530

    Create  find whether the climate is HOT or COLD.cpp
    Is it hot or cold
    Chef considers the climate HOT if the temperature is above 
    20
    20, otherwise he considers it COLD. You are given the temperature 
    C
    C, find whether the climate is HOT or COLD.
    
    Input Format
    The first line of input will contain a single integer 
    T
    T, denoting the number of test cases.
    The first and only line of each test case contains a single integer, the temperature 
    C
    C.
    Output Format
    For each test case, print on a new line whether the climate is HOT or COLD.
    
    You may print each character of the string in either uppercase or lowercase (for example, the strings hOt, hot, Hot, and HOT will all be treated as identical).
    
    Constraints
    1
    ≤
    T
    ≤
    50
    1≤T≤50
    0
    ≤
    C
    ≤
    40
    0≤C≤40
    Sample 1:
    Input
    Output
    2
    21
    16
    HOT
    COLD
    Explanation:
    Test case 
    1
    1: The temperature is 
    21
    21, which is more than 
    20
    20. So, Chef considers the climate HOT.
    
    Test case 
    2
    2: The temperature is 
    16
    16, which is not more than 
    20
    20. So, Chef considers the climate COLD.

---
+++ find whether the climate is HOT or COLD.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,c;
+	cin>>t;
+	while(t--){
+	    cin>>c;
+	    if(c>20){
+	        cout<<"HOT"<<endl;
+	    }
+	    else{
+	    cout<<"cold"<<endl;
+	        
+	    }
+	}
+
+}
