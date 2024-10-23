591c7e50703181f78463582b74549fea677edf70
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2024-10-23 20:38:03 +0530

    Create Can you decide which frame Chef will buy.cpp
    Glass Prices
    Chef is buying spectacles, and is now deciding on which frame to purchase.
    
    Chef has narrowed his options down to two choices: a plastic frame that costs 
    X
    X rupees, and a metal frame that costs 
    Y
    Y rupees.
    
    Chef will buy the metal frame if and only if it costs at most twice the price of the plastic frame - otherwise he will buy the plastic frame.
    
    Can you decide which frame Chef will buy?
    
    Input Format
    The first and only line of input will contain two integers 
    X
    X and 
    Y
    Y - the price of the plastic frame and the metal frame, respectively.
    Output Format
    For each test case, output on a new line the answer: the type of frame Chef will purchase.
    This is either the string "PLASTIC" or the string "METAL" (without quotes) depending on the outcome.
    
    Each letter of the output may be printed in either uppercase or lowercase - for example, the strings METAL, MeTaL, metal, and mEtAL will all be treated as equivalent.
    
    Constraints
    1
    ≤
    X
    ≤
    2000
    1≤X≤2000
    1
    ≤
    Y
    ≤
    2000
    1≤Y≤2000
    Sample 1:
    Input
    Output
    499 999
    PLASTIC
    Explanation:
    The plastic frame costs 
    499
    499 rupees, and the metal one costs 
    999
    999 rupees.
    
    2
    ×
    499
    =
    998
    2×499=998 is less than 
    999
    999, so Chef will prefer the plastic frame.
    
    Sample 2:
    Input
    Output
    499 998
    METAL
    Explanation:
    The plastic frame costs 
    499
    499 rupees, and the metal one costs 
    998
    998 rupees.
    
    2
    ×
    499
    =
    998
    2×499=998 is equal to 
    998
    998, so Chef will prefer the metal frame.
    
    Sample 3:
    Input
    Output
    698 1099
    METAL
    Explanation:
    The plastic frame costs 
    698
    698 rupees, and the metal one costs 
    1099
    1099 rupees.
    
    2
    ×
    698
    =
    1396
    2×698=1396 is greater than 
    1099
    1099, so Chef will prefer the metal frame.
    
    accepted
    Accepted
    18031
    total-Submissions
    Submissions
    32716
    accuracy
    Accuracy
    66.28
    Did you like the problem statement?
    19 users found this helpful
    More Info
    Time limit1 secs
    Memory limit1.5 GB
    Source Limit50000 Bytes

---
+++ Can you decide which frame Chef will buy.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x,y;
+	cin>>x>>y;
+	if(x*2 < y){
+	    cout<<"PLASTIC"<<endl;
+	}
+	else{
+	    cout<<"METAL"<<endl;
+	}
+	return 0;
+}
